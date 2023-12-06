84a41a5e676ecafeafca4c91981a17ae39d69163
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2023-12-05 20:43:45 -0500

    updating db

--- inventory/models.py
+++ inventory/models.py
@@ -2,9 +2,11 @@
 from django.db import models
 
 class InventoryItem(models.Model):
+    expire_date = models.DateField
     name = models.CharField(max_length=255)
     description = models.TextField()
     quantity = models.PositiveIntegerField()
+    ndc_number = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
